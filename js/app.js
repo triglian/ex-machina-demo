92364a1bc0d23651df0753369db75f4932a95a38
Vassilis Triglianos <triglian@gmail.com>
2018-11-27 17:04:03 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+import products from './products';
+
+const app = {
+  init() {
+    this.renderProducts();
+  },
+
+  renderProducts() {
+    const ulEl = document.getElementById('product-list');
+    const tEl = document.getElementById('product-row');
+
+    products.forEach(p => {
+      const clone = document.importNode(tEl.content, true);
+
+      // using indexes would be faster but more error prone
+      // if we change the templated
+      const imgEl = clone.querySelector('div.product-item-image img');
+      imgEl.src = p.image;
+      imgEl.alt = `${p.name} image`;
+      clone.querySelector('div.product-item-name').textContent = p.name;
+      clone.querySelector('div.product-item-price').textContent = p.price;
+      clone
+        .querySelector('div.product-item-add-to-cart > button')
+        .setAttribute('data-item-id', p.id);
+
+      ulEl.appendChild(clone);
+    });
+  }
+};
+
+export default app;
