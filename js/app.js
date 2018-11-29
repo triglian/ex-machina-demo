08bc962d0888b945aac1f344faf77108853c119e
Vassilis Triglianos <triglian@gmail.com>
2018-11-29 21:13:01 +0100

    switch between grid and list layout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,44 @@ import products from './products.js';
 
 const app = {
   init() {
+    this.setupEventListeners();
     this.renderProducts();
+    this.setProductView('grid');
+  },
+
+  setupEventListeners(){
+    // one click handler for all click events we're interested in
+    // we use event bubbling to reduce the memory footprint for
+    // handling events
+    document.addEventListener('click', event => {
+
+      // view toggle buttons
+      if (event.target.id === 'list-view-btn' ||
+        (event.target.parentElement && event.target.parentElement.id === 'list-view-btn')){
+        return this.setProductView('list');
+      }
+
+      if (event.target.id === 'grid-view-btn' ||
+        (event.target.parentElement && event.target.parentElement.id === 'grid-view-btn')){
+        return this.setProductView('grid');
+      }
+
+      return null;
+    });
+  },
+
+  setProductView(viewStyle){
+    if(viewStyle === 'grid'){
+      document.getElementById('list-view-btn').classList.remove('active');
+      document.getElementById('grid-view-btn').classList.add('active');
+      document.getElementById('product-list').classList.remove('list-view');
+      document.getElementById('product-list').classList.add('grid-view');
+    }else if(viewStyle === 'list'){
+      document.getElementById('grid-view-btn').classList.remove('active');
+      document.getElementById('list-view-btn').classList.add('active');
+      document.getElementById('product-list').classList.remove('grid-view');
+      document.getElementById('product-list').classList.add('list-view');
+    }
   },
 
   renderProducts() {
